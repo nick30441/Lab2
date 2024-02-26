3a0630f39bd754462290c94433570485366b6755
nick30441 <nicholas.novak@okstate.edu>
2024-02-26 13:33:50 -0600

    Day1

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -28,6 +28,11 @@ module GenerateKeys (Key, SubKey1, SubKey2, SubKey3, SubKey4,
    output logic [47:0] SubKey15;
    output logic [47:0] SubKey16;
 
+   PC1 mod1(Key, left_block, right_block);
+   assign left_block = {left_block[54:28], left_block[55]};
+   assign right_block = {right_block[26:0], right_block[27]};
+   PC2 mod2(left_block, right_block, subkey);
+
 endmodule // GenerateKeys
 
 module PC1 (key, left_block, right_block);
@@ -36,8 +41,70 @@ module PC1 (key, left_block, right_block);
    output logic [27:0] left_block;
    output logic [27:0] right_block;
 
-   logic [55:0]        out_block;
+   logic [55:0] out_block;
 
+//left
+   assign out_block[55] = key[64-57];
+   assign out_block[54] = key[64-49];
+   assign out_block[53] = key[64-41];
+   assign out_block[52] = key[64-33];
+   assign out_block[51] = key[64-25];
+   assign out_block[50] = key[64-17];
+   assign out_block[49] = key[64-9];
+   assign out_block[48] = key[64-1];
+   assign out_block[47] = key[64-58];
+   assign out_block[46] = key[64-50];   
+   assign out_block[45] = key[64-42];   
+   assign out_block[44] = key[64-34];
+   assign out_block[43] = key[64-26];
+   assign out_block[42] = key[64-18];
+   assign out_block[41] = key[64-10];
+   assign out_block[40] = key[64-2];
+   assign out_block[39] = key[64-59];
+   assign out_block[38] = key[64-51];
+   assign out_block[37] = key[64-43];
+   assign out_block[36] = key[64-35];   
+   assign out_block[35] = key[64-27];
+   assign out_block[34] = key[64-19];   
+   assign out_block[33] = key[64-11];
+   assign out_block[32] = key[64-3];
+   assign out_block[31] = key[64-60];
+   assign out_block[30] = key[64-52];
+   assign out_block[29] = key[64-44];
+   assign out_block[28] = key[64-36];
+   //right
+   assign out_block[27] = key[64-63];
+   assign out_block[26] = key[64-55];   
+   assign out_block[25] = key[64-47];
+   assign out_block[24] = key[64-39];   
+   assign out_block[23] = key[64-31];
+   assign out_block[22] = key[64-23];
+   assign out_block[21] = key[64-15];
+   assign out_block[20] = key[64-7];
+   assign out_block[19] = key[64-62];
+   assign out_block[18] = key[64-54];
+   assign out_block[17] = key[64-46];   
+   assign out_block[16] = key[64-38];   
+   assign out_block[15] = key[64-30];
+   assign out_block[14] = key[64-22];   
+   assign out_block[13] = key[64-14];
+   assign out_block[12] = key[64-6];
+   assign out_block[11] = key[64-61];
+   assign out_block[10] = key[64-53];
+   assign out_block[9] = key[64-45];
+   assign out_block[8] = key[64-37];
+   assign out_block[7] = key[64-29];
+   assign out_block[6] = key[64-21];   
+   assign out_block[5] = key[64-13];
+   assign out_block[4] = key[64-5];   
+   assign out_block[3] = key[64-28];
+   assign out_block[2] = key[64-20];
+   assign out_block[1] = key[64-12];
+   assign out_block[0] = key[64-4];
+
+   assign left_block = out_block[55:28];
+   assign right_block = out_block[27:0];
+   
 endmodule // PC1
 
 module PC2 (left_block, right_block, subkey);
@@ -45,6 +112,59 @@ module PC2 (left_block, right_block, subkey);
    input logic [27:0] left_block;
    input logic [27:0] right_block;
    output logic [47:0] subkey;
+   
+   logic [47:0] combined;
+
+   assign combined = {left_block, right};
+   
+   assign subkey[47] = combined[64-14];
+   assign subkey[46] = combined[64-17];
+   assign subkey[45] = combined[64-11];
+   assign subkey[44] = combined[64-24];   
+   assign subkey[43] = combined[64-1];
+   assign subkey[42] = combined[64-5];   
+   assign subkey[41] = combined[64-3];
+   assign subkey[40] = combined[64-28];
+   assign subkey[39] = combined[64-15];
+   assign subkey[38] = combined[64-6];
+   assign subkey[37] = combined[64-21];
+   assign subkey[36] = combined[64-10];
+   assign subkey[35] = combined[64-23];
+   assign subkey[34] = combined[64-19];   
+   assign subkey[33] = combined[64-12];
+   assign subkey[32] = combined[64-4];   
+   assign subkey[31] = combined[64-26];
+   assign subkey[30] = combined[64-8];
+   assign subkey[29] = combined[64-16];
+   assign subkey[28] = combined[64-7];
+   assign subkey[27] = combined[64-27];
+   assign subkey[26] = combined[64-20];
+   assign subkey[25] = combined[64-13];   
+   assign subkey[24] = combined[64-2];   
+   assign subkey[23] = combined[64-41];
+   assign subkey[22] = combined[64-52];   
+   assign subkey[21] = combined[64-31];
+   assign subkey[20] = combined[64-37];
+   assign subkey[19] = combined[64-47];
+   assign subkey[18] = combined[64-55];
+   assign subkey[17] = combined[64-30];
+   assign subkey[16] = combined[64-40];
+   assign subkey[15] = combined[64-51];
+   assign subkey[14] = combined[64-45];   
+   assign subkey[13] = combined[64-33];
+   assign subkey[12] = combined[64-48];   
+   assign subkey[11] = combined[64-44];
+   assign subkey[10] = combined[64-49];
+   assign subkey[9] = combined[64-39];
+   assign subkey[8] = combined[64-56];
+   assign subkey[7] = combined[64-34];
+   assign subkey[6] = combined[64-53];
+   assign subkey[5] = combined[64-46];
+   assign subkey[4] = combined[64-42];   
+   assign subkey[3] = combined[64-50];
+   assign subkey[2] = combined[64-36];    
+   assign subkey[1] = combined[64-29];
+   assign subkey[0] = combined[64-32];
 
 endmodule // PC2
 

1f640e832fb252a247071274ba883d04c43267ed
Novak <nicholas.novak@okstate.edu>
2024-03-13 20:37:19 -0500

    ALLLL DONE

--- Lab2/Vivado/Lab1/Lab1.runs/impl_1/top_demo.tcl
+++ Lab2/Vivado/Lab1/Lab1.runs/impl_1/top_demo.tcl
@@ -115,8 +115,6 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
@@ -125,10 +123,7 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 4
-  set_param synth.incrementalSynthesisCache C:/Users/nicnova/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-25644-CEAT-ENDV350-02/incrSyn
-  set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 8  }
   open_checkpoint top_demo_routed.dcp
   set_property webtalk.parent_dir C:/Users/nicnova/Lab2/Lab2/Vivado/Lab1/Lab1.cache/wt [current_project]
